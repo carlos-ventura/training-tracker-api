64ea1935eb5277c38485f270acb9152f9a2ff3d4
carlos-ventura <carlospinho_pt@hotmail.com>
2023-12-11 19:53:35 +0100

    Create FastAPI main.py

---
+++ app/main.py
@@ -0,0 +1,66 @@
+from contextlib import asynccontextmanager
+
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+
+from app.constants import DESCRIPTION
+from app.db.setup import Base, engine, session
+from app.enums import TrainingGame, TrainingType
+from app.models import Training
+from app.tasks import (
+    delete_training_task,
+    load_all_training_task,
+    load_trainings_game_task,
+    load_trainings_type_task,
+    save_training_task,
+)
+
+
+def get_db_session():
+    db_session = session()
+    try:
+        yield db_session
+    finally:
+        db_session.close()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    conn = engine.connect()
+    try:
+        Base.metadata.create_all(bind=engine)
+        yield
+    finally:
+        conn.close()
+
+
+app = FastAPI(
+    title="Training Tracker",
+    description=DESCRIPTION,
+    lifespan=lifespan
+)
+
+
+@app.get("/trainings/")
+def load_trainings(db_session: Session = Depends(get_db_session)):
+    return load_all_training_task(db_session)
+
+
+@app.get("/trainings/game")
+def load_training_game(game: TrainingGame, db_session: Session = Depends(get_db_session)):
+    return load_trainings_game_task(game, db_session)
+
+
+@app.get("/training/type")
+def load_training_type(training_type: TrainingType, db_session: Session = Depends(get_db_session)):
+    return load_trainings_type_task(training_type, db_session)
+
+
+@app.post("/trainings/")
+def save_training(training: Training, db_session: Session = Depends(get_db_session)):
+    save_training_task(training, db_session)
+
+
+@app.delete("/trainings/{training_id}")
+def delete_training(training_id: str, db_session: Session = Depends(get_db_session)):
+    delete_training_task(training_id, db_session)
