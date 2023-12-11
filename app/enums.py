50aa58a4ab3da188dbafe4bdfbbe4f56ca712979
carlos-ventura <carlospinho_pt@hotmail.com>
2023-12-11 19:51:54 +0100

    Create enums.py

---
+++ app/enums.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+
+class TrainingGame(str, Enum):
+    RocketLeague = "rocket_league"
+    LeagueOfLegends = "league_of_legends"
+
+
+class TrainingType(str, Enum):
+    AirRoll = "air_roll"
+    Flips = "flips"
+
+
+class TrainingSubType(str, Enum):
+    # AirRoll
+    Q1 = "q1"
+    Q2 = "q2"
+    Q3 = "q3"
+    Q4 = "q4"
+
+    # Flips
+    HalfFlip = "half_flip"
+    SpeedFlip = "speed_flip"
