6c986644f8f729d599229d6b2d065e1359f87746
carlos-ventura <carlospinho_pt@hotmail.com>
2024-01-21 17:30:51 +0100

    Add default examples

--- app/models.py
+++ app/models.py
@@ -1,13 +1,13 @@
 from datetime import date as date_datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from app.enums import TrainingGame, TrainingSubType, TrainingType
 
 
 class Training(BaseModel):
-    game: TrainingGame
-    type: TrainingType
-    sub_type: TrainingSubType
+    game: TrainingGame = Field(examples=[TrainingGame.RocketLeague])
+    type: TrainingType = Field(examples=[TrainingType.Flips])
+    sub_type: TrainingSubType = Field(examples=[TrainingSubType.SpeedFlip])
     hours: float
     date: date_datetime = date_datetime.today()
