324022860a74db5690ed5b64f41d40525332fdc2
Carlos Ventura Old Desktop <carlospinho_pt@hotmail.com>
2025-01-03 23:52:01 +0000

    Comment enums and add Skate

--- app/enums.py
+++ app/enums.py
@@ -2,35 +2,46 @@ from enum import Enum
 
 
 class TrainingGame(str, Enum):
-    RocketLeague = "rocket_league"
-    LeagueOfLegends = "league_of_legends"
+    # RocketLeague = "rocket_league"
+    # LeagueOfLegends = "league_of_legends"
+    Skate = "skate"
 
 
 class TrainingType(str, Enum):
-    AirRoll = "air_roll"
-    Flips = "flips"
-    Control = "control"
-    Dash = "dash"
+    # SKATE
+    Ground = "ground"
+    Rails = "rails"
+    Bowl = "bowl"
 
+    # ROCKET LEAGUE
+    # AirRoll = "air_roll"
+    # Flips = "flips"
+    # Control = "control"
+    # Dash = "dash"
 
 class TrainingSubType(str, Enum):
-    # AirRoll
-    Q1 = "q1"
-    Q2 = "q2"
-    Q3 = "q3"
-    Q4 = "q4"
-    Shot = "shot"
-    TornadoSping = "tornado_spin"
+    # SKATE
+    ShoveIt = "shove_it"
 
-    # Flips
-    HalfFlip = "half_flip"
-    SpeedFlip = "speed_flip"
+    # ROCKET LEAGUE
+    # # AirRoll
+    # Q1 = "q1"
+    # Q2 = "q2"
+    # Q3 = "q3"
+    # Q4 = "q4"
+    # Shot = "shot"
+    # TornadoSping = "tornado_spin"
 
-    # Control
-    GroundControl = "ground_control"
-    Flicks = "flicks"
+    # # Flips
+    # HalfFlip = "half_flip"
+    # SpeedFlip = "speed_flip"
 
-    # Dash
-    WallDash = "wall_dash"
-    ZapDash = "zap_dash"
-    WaveDash = "wave_dash"
+    # # Control
+    # GroundControl = "ground_control"
+    # Flicks = "flicks"
+
+    # # Dash
+    # WallDash = "wall_dash"
+    # ZapDash = "zap_dash"
+    # WaveDash = "wave_dash"
+
