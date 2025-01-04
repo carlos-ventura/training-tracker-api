1a070ef9e7c9664e84349b9ccafd6cf337dbdd02
Carlos Ventura Old Desktop <carlospinho_pt@hotmail.com>
2025-01-04 01:18:29 +0000

    Simplify auth.py

--- app/auth.py
+++ app/auth.py
@@ -1,10 +1,7 @@
-from fastapi import HTTPException, Request, Security
-from fastapi.security import api_key
+from fastapi import HTTPException, Request
 import os
 
-api_key_header = api_key.APIKeyHeader(name="X-API-KEY")
 
-
-async def validate_api_key(key: str = Security(api_key_header)):
-    if key != os.getenv("API_KEY"):
+async def validate_api_key(request: Request):
+    if request.cookies.get("api_key") != os.getenv("API_KEY"):
          raise HTTPException(status_code=401, detail="Unauthorized")
